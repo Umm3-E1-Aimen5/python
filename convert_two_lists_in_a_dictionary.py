5c91cac939924a2140639598ba0999eb68ec2efa
ummeaimen <hafiza.aimen786@gmail.com>
2025-06-24 13:00:09 +0500

    convert lists into dictionary

---
+++ convert_two_lists_in_a_dictionary.py
@@ -0,0 +1,8 @@
+numbers = [1,2,3,4]
+names = ['Aimen','Asia','Fatima','Urooj']
+combine_list = {}
+for i in range(len(numbers)):
+    number = numbers[i]
+    name = names[i]
+    combine_list[number] = name
+print(combine_list)    
